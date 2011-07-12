28acbf3e2c95c24863be996d54245c7ada088877
Morbus Iff <morbus@disobey.com>
2011-07-11 21:26:47 -0400

    404 bugfix and adminterface to give or take achievements to/from a user.

--- achievements.api.php
+++ achievements.api.php
@@ -22,6 +22,11 @@
  *   - title: (required) The title of the achievement.
  *   - description: (required) A description of the achievement.
  *   - points: (required) How many points the user will earn when unlocked.
+ *   - storage: (optional) If you store statistics for your achievement, the
+ *     core module assumes you've used the achievement ID for the storage
+ *     location. If you haven't, specify the storage location here. This lets
+ *     the core module know what to delete when an administrator manually
+ *     removes an achievement unlock from a user.
  *   - hidden: (optional) The achievement is a sekrit until it is unlocked.
  */
 function hook_achievements_info() {
@@ -38,10 +43,18 @@ function hook_achievements_info() {
       'description' => t('But what about the children?!'),
       'points'      => 100,
     ),
+    'node-mondays-1' => array(
+      'id'          => 'node-mondays-1',
+      'title'       => t('Published some content on a Monday'),
+      'description' => t("Go back to bed: it's still the weekend!"),
+      'storage'     => 'node-mondays',
+      'points'      => 5,
+    ),
     'node-mondays-2' => array(
       'id'          => 'node-mondays-2',
-      'title'       => t('Created a node on two separate Mondays.'),
+      'title'       => t('Published content on two separate Mondays'),
       'description' => t('Garfield hates you. Hates youuUUuUuu!'),
+      'storage'     => 'node-mondays',
       'points'      => 10,
     ),
   );
@@ -51,42 +64,19 @@ function hook_achievements_info() {
  * Implements hook_comment_insert().
  */
 function example_comment_insert($comment) {
-  // Most achievements measure some kind of statistical data that must be
-  // aggregated over time. To ease the storage of this data, the achievement
-  // module ships with achievement_storage_get() and _set(), which allow you
-  // to store custom data on a per-user basis. In most cases, the storage
-  // key is the same as your achievement ID but in situations where you have
-  // progressive achievements (10, 20, 50, 100 comments etc.), it's better
-  // to use a single key shared amongst them.
+  // Here's a really simple achievement to start with: commenting unlocks.
+  // First, count the number of this user's published comments and add 1.
+  $current_count = db_select('comment')->condition('uid', $comment->uid)->condition('status', 1)->countQuery()->execute()->fetchField() + 1;
 
-  // Here we're grabbing the number of comments that the current commenter has
-  // left in the past (which might be 0), adding 1 (for the current insert),
-  // and then saving the count back to the database. The saved data is
-  // serialized so can be as simple or as complex as you need it.
-  $current_count = achievements_storage_get('comment-count') + 1;
-  achievements_storage_set('comment-count', $current_count);
-
-  // Note that we're not checking if the user has previously earned any of the
-  // commenting achievements yet. There are two reasons: first, we might want
-  // to add another commenting achievement for, say, 250 comments, and if we
-  // had stopped the storage counter above at 100, someone who currently has
-  // 300 comments wouldn't unlock the achievement until they added another 150
-  // nuggets of wisdom to the site. Generally speaking, if you need to store
-  // incremental data for an achievement, you should continue to store it even
-  // after the achievement has been unlocked - you never know if you'll want
-  // to add a future milestone that will unlock on higher increments.
-  //
-  // Secondly, the achievements_unlocked() function below will automatically
-  // check if the user has unlocked the achievement already, and will not
-  // unlock it again if they have. This saves you a small bit of repetitive
-  // coding but you're welcome to use achievements_unlocked_already() should
-  // the need every arise.
-  //
-  // Knowing that we currently have 50 and 100 comment achievements, we simply
-  // loop through each milestone and check if the current count value matches.
+  // Knowing the user's current count and that we've defined achievements
+  // for 50 and 100 comments, we can simply loop through those two numbers
+  // and issue an unlock. The achievements_unlocked() function will check if
+  // the user has unlocked the achievement already and will not reward it
+  // again if they have. This saves you a small bit of repetitive coding but
+  // you're welcome to use achievements_unlocked_already() as needed.
   foreach (array(50, 100) as $count) {
     if ($current_count == $count) {
-      achievements_unlocked('comment-count-' . $count);
+      achievements_unlocked('comment-count-' . $count, $comment->uid);
     }
   }
 }
@@ -95,16 +85,34 @@ function example_comment_insert($comment) {
  * Implements hook_node_insert().
  */
 function example_node_insert($node) {
-  // This is very similar to the comment counting achievement, but shows
-  // storage of an array and a slightly different unlocking check. Yawn.
+  // Most achievements measure some kind of statistical data that must be
+  // aggregated over time. To ease the storage of this data, the achievement
+  // module ships with achievement_storage_get() and _set(), which allow you
+  // to store custom data on a per-user basis. In most cases, the storage
+  // location is the same as your achievement ID but in situations where you
+  // have progressive achievements (1, 2, 5 Mondays etc.), it's better to
+  // share a single place like we do below. If you don't use the achievement
+  // ID for the storage location, you must specify the new location in the
+  // 'storage' key of hook_achievements_info().
   if (format_date(REQUEST_TIME, 'custom', 'D') == 'Mon') {
-    $current_mondays = achievements_storage_get('node-mondays');
+    $current_mondays = achievements_storage_get('node-mondays', $node->uid);
     $current_mondays = is_array($current_mondays) ? $current_mondays : array();
     $current_mondays[format_date(REQUEST_TIME, 'custom' 'Y-m-d')] = 1;
-    achievements_storage_set('node-mondays', $current_mondays);
+    achievements_storage_set('node-mondays', $current_mondays, $node->uid);
 
-    if (count($current_mondays) == 2) {
-      achievements_unlocked('node-mondays-2');
+    // Note that we're not checking if the user has previously earned any of
+    // the Monday achievements. Primarily, this is because we might want to
+    // add another Monday achievement for 10 separate Mondays, and if we had
+    // stopped the storage counter above at 2, someone who happened to post on
+    // 20 other Mondays wouldn't unlock the achievement until they added more
+    // content on yet 8 more Mondays. Generally speaking, if you need to store
+    // incremental data for an achievement, you should continue to store it
+    // even after all achievements have been unlocked - you never know if
+    // you'll want a future milestone that will unlock on higher increments.
+    foreach (array(1, 2) as $count) {
+      if (count($current_mondays) == $count) {
+        achievements_unlocked('node-mondays-' . $count, $node->uid);
+      }
     }
   }
 }
