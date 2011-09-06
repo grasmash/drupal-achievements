7aafb4080348b880f092cee2676a4c186ac012c5
Morbus Iff <morbus@disobey.com>
2011-09-06 13:25:31 -0400

    JS popups can now handle multiple achievements, and unlocked achievements can now be configured to remain in defined order (versus being sorted to the top).

---
+++ achievement-notification.tpl.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation for an achievement notification popup.
+ *
+ * Available variables:
+ * - $achievement: The achievement being displayed, as an array.
+ * - $unlock: An array of unlocked 'rank' and 'timestamp', if applicable.
+ * - $unlocked_date: A formatted date of the user's unlock timestamp.
+ * - $unlocked_rank: A formatted string of the user's unlock ranking.
+ * - $image: The achievement's image (default or otherwise).
+ * - $achievement_url: Direct URL of the current achievement.
+ * - $classes: String of classes for this achievement.
+ */
+?>
+<div class="<?php print $classes; ?>">
+  <div class="achievement-image"><?php print $image; ?></div>
+
+  <div class="achievement-points-box">
+    <div class="achievement-points"><?php print $achievement['points']; ?></div>
+    <div class="achievement-unlocked-stats">
+      <div class="achievement-unlocked-rank"><?php print $unlocked_rank; ?></div>
+    </div>
+  </div>
+
+  <div class="achievement-body">
+    <div class="achievement-header">Achievement Unlocked</div>
+    <div class="achievement-title"><a href="<?php print $achievement_url; ?>" tabindex="-1"><?php print $achievement['title']; ?></a></div>
+  </div>
+</div>
