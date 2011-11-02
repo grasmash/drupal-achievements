a4aa19b32b3a53a06c7b39cb59760d2cc8ee4694
Morbus Iff <morbus@disobey.com>
2011-11-02 12:45:58 -0400

    Most .tpl.php  are now render arrays instead of strings.

--- achievement-notification.tpl.php
+++ achievement-notification.tpl.php
@@ -7,26 +7,28 @@
  * Available variables:
  * - $achievement: The achievement being displayed, as an array.
  * - $unlock: An array of unlocked 'rank' and 'timestamp', if applicable.
- * - $unlocked_date: A formatted date of the user's unlock timestamp.
- * - $unlocked_rank: A formatted string of the user's unlock ranking.
- * - $image: The achievement's image (default or otherwise), linked.
- * - $image_raw: The raw path to the context-aware achievement image.
+ * - $unlocked_date: A renderable string of the user's unlock timestamp.
+ * - $unlocked_rank: A renderable string of the user's unlock ranking.
+ * - $image: The renderable achievement's linked image (default or otherwise).
+ * - $image_path: The raw path to the context-aware achievement image.
  * - $achievement_url: Direct URL of the current achievement.
+ * - $achievement_title: The renderable and linked achievement title.
+ * - $state: The 'unlocked', 'locked', or 'hidden' achievement state.
  * - $classes: String of classes for this achievement.
  */
 ?>
 <div class="<?php print $classes; ?>">
-  <div class="achievement-image"><?php print $image; ?></div>
+  <div class="achievement-image"><?php print render($image); ?></div>
 
   <div class="achievement-points-box">
     <div class="achievement-points"><?php print $achievement['points']; ?></div>
     <div class="achievement-unlocked-stats">
-      <div class="achievement-unlocked-rank"><?php print $unlocked_rank; ?></div>
+      <div class="achievement-unlocked-rank"><?php print render($unlocked_rank); ?></div>
     </div>
   </div>
 
   <div class="achievement-body">
     <div class="achievement-header">Achievement Unlocked</div>
-    <div class="achievement-title"><a href="<?php print $achievement_url; ?>"><?php print $achievement['title']; ?></a></div>
+    <div class="achievement-title"><?php print render($achievement_title); ?></div>
   </div>
 </div>
